326d4555d50a14b105a8fb73d89d308ffcb9ada3
Kemoemadal3rabwy <kareememad2007925@gmail.com>
2026-01-29 02:10:00 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
-<script>
-const supabaseUrl = "https://xcxaxcitexzfbsgqwnjw.supabase.co";
-const supabaseKey = "sb_publishable_RE8lxgFvza5sHFgTt_RpDQ_erGH_A13";
-
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-</script>
